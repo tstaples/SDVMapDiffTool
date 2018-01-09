51d8b48a2768e3cad1fe6022d272f19bb1780190
tyler staples <tyler.a.staples@gmail.com>
2018-01-08 22:54:47 -0800

    Added PathResolvingTileSheetGroup and set minimum api version to 2.4 for the json fix.

--- MapEditorMod/MapEditor.cs
+++ MapEditorMod/MapEditor.cs
@@ -7,31 +7,16 @@ using System.IO;
 using xTile;
 using StardewValley;
 using StardewModdingAPI.Events;
-using Newtonsoft.Json;
-using xTile.Display;
-using Microsoft.Xna.Framework.Graphics;
-using xTile.Dimensions;
-using xTile.Tiles;
-using Newtonsoft.Json.Converters;
 
-namespace MapEditorMod
+namespace MapEditor
 {
-    public class TestTileSheetGroup : SeasonalTileSheetGroup
-    {
-        public IModHelper Helper;
-
-        public override string GetTileSheetPath()
-        {
-            return this.Helper.Content.GetActualAssetKey(base.GetTileSheetPath());
-        }
-    }
-
     public class MapEditorMod : Mod
     {
         public override void Entry(IModHelper helper)
         {
             SaveEvents.AfterLoad += SaveEvents_AfterLoad;
 
+            // Temp for testing
             InputEvents.ButtonPressed += (sender, e) =>
             {
                 if (e.Button == SButton.F7)
@@ -44,34 +29,12 @@ namespace MapEditorMod
             const string TileSheetId = "zpathtotalbathhouseoverhaulexterior";
 
             string diffPath = Path.Combine(Helper.DirectoryPath, "TestData/diff.json");
-            string data = File.ReadAllText(diffPath);
-
-            /////////////////////////////////////////////////////////////////////////////////
-            // BEGIN CONFUSION
-            /////////////////////////////////////////////////////////////////////////////////
-            var settings = new JsonSerializerSettings
-            {
-                Formatting = Formatting.Indented,
-                ObjectCreationHandling = ObjectCreationHandling.Replace,
-                Converters = new List<JsonConverter> { new StringEnumConverter() }
-            };
-
-            // Works
-            Diff mapDiff = JsonConvert.DeserializeObject<Diff>(data, settings);
-
-            // Doesn't work
-            //string jsonPath = Path.Combine(/*this.Helper.DirectoryPath,*/ "TestData", "diff.json");
-            //Diff mapDiff = this.Helper.ReadJsonFile<Diff>(jsonPath);
-
-            /////////////////////////////////////////////////////////////////////////////////
-            // END CONFUSION
-            /////////////////////////////////////////////////////////////////////////////////
+            Diff mapDiff = this.Helper.ReadJsonFile<Diff>(diffPath);
 
             Map targetMap = Game1.getLocationFromName("Railroad").map;
 
-            TestTileSheetGroup tileSheetGroup = new TestTileSheetGroup()
+            PathResolvingTileSheetGroup tileSheetGroup = new PathResolvingTileSheetGroup(this.Helper)
             {
-                Helper = this.Helper,
                 UniqueId = TileSheetId,
                 TileSheetPaths = Enum.GetValues(typeof(Season))
                 .Cast<Season>()
@@ -79,7 +42,7 @@ namespace MapEditorMod
                 .ToDictionary(k => k.Key, v => v.Value)
             };
 
-            IMapEditor mapEditor = new MapEditor();
+            IMapEditor mapEditor = new MapEditorFramework.MapEditor();
             var tileSheetProvider = new TileSheetProvider(tileSheetGroup);
             mapEditor.Patch(targetMap, mapDiff, tileSheetProvider, Game1.mapDisplayDevice);
         }
---
+++ MapEditorMod/PathResolvingTileSheetGroup.cs
@@ -0,0 +1,21 @@
+﻿using MapEditorFramework;
+using StardewModdingAPI;
+
+namespace MapEditor
+{
+    internal class PathResolvingTileSheetGroup : SeasonalTileSheetGroup
+    {
+        public IModHelper Helper;
+
+        public PathResolvingTileSheetGroup(IModHelper helper)
+        {
+            this.Helper = helper;
+        }
+
+        public override string GetTileSheetPath()
+        {
+            // Resolve the proper path.
+            return this.Helper.Content.GetActualAssetKey(base.GetTileSheetPath());
+        }
+    }
+}
